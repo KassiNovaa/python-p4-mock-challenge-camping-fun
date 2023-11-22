05dd95500e0e3fe9926c8f8eccd33fe96cba29f4
Kassidy Puello <kassidympuello@gmail.com>
2023-11-22 09:58:12 -0500

    fin

--- server/app.py
+++ server/app.py
@@ -20,6 +20,95 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api( app )
+
+class Campers( Resource ):
+    def post( self ):
+        params = request.json
+        try:
+            camper = Camper( name = params[ 'name' ], age = params[ 'age' ] )
+        except:
+            return make_response( { 'error': [ 'user messed up' ] }, 422 )
+        db.session.add( camper )
+        db.session.commit()
+        return make_response( camper.to_dict( rules = ( '-signups', ) ), 201 )
+
+    def get( self ):
+        #without nested info add rules into the to_dict
+        all_campers = [ c.to_dict( rules = ('-signups',) ) for c in Camper.query.all() ]
+        return make_response( all_campers, 200 )
+    
+api.add_resource( Campers, '/campers'  )
+
+class CampersById( Resource ):
+    
+    def get( self, id ):
+        camper = Camper.query.get( id )
+        if not camper:
+            return make_response( { 'error': 'camper not found' }, 404 )
+        return make_response( camper.to_dict(), 200 )
+
+    def patch( self, id ):
+        camper = Camper.query.get( id )
+        if not camper:
+            return make_response( { 'error': 'camper not found' }, 404 )
+        params = request.json
+        try:
+            for attr in params:
+                setattr( camper, attr, params[attr] )
+        except:
+            return make_response( { 'error': [ 'user messed up' ] }, 422 )
+        db.session.commit()
+
+        return make_response( camper.to_dict( rules = ( '-signups', ) ), 200 )
+
+
+api.add_resource( CampersById, '/campers/<id>')
+
+class Activities( Resource ):
+    def get( self ):
+        return make_response( 
+            [ a.to_dict() for a in Activity.query.all() ], 200 
+        )
+
+api.add_resource( Activities, '/activities' )
+
+class ActivitiesById( Resource ):
+    def delete( self, id ):
+        activity = Activity.query.get( id  )
+        if not activity:
+            return make_response( { 'error': 'Activity not found' }, 404 )
+
+        db.session.delete( activity )
+        db.session.commit()
+        return make_response( "it won't matter what we put here", 204 )
+
+api.add_resource( ActivitiesById, '/activities/<id>' )
+
+class Signups( Resource ):
+    def post( self ):
+        params = request.json
+        try:
+            signup = Signup( 
+                time = params[ 'time' ], 
+                camper_id = params[ 'camper_id'],
+                activity_id = params[ 'activity_id' ]
+            )
+        except:
+            return make_response( { 'error': [ 'all bad!' ] }, 422 )
+        db.session.add( signup )
+        db.session.commit()
+        return make_response( 
+            signup.to_dict( rules = (
+                'id', 'camper_id', 'activity_id', 'time', 
+                'activity', 'camper',
+                '-activity.signups', '-camper.signups'
+            ) ), 
+            201 
+        )
+
+api.add_resource( Signups, '/signups' )
+
 
 @app.route('/')
 def home():
--- server/models.py
+++ server/models.py
@@ -20,13 +20,19 @@ db = SQLAlchemy(metadata=metadata)
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
+    # Add serialization rules
+
+    serialize_rules = ( '-signups.activity', )
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
-    # Add serialization rules
+
+    signups = db.relationship( 'Signup' , back_populates = 'activity', cascade = 'all, delete-orphan' )
+    campers = association_proxy( 'signups' , 'camper' )
+
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -35,16 +41,35 @@ class Activity(db.Model, SerializerMixin):
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
+    # Add serialization rules
+
+    serialize_rules  = ('-signups.camper',)
+
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship( 'Signup' , back_populates = 'camper' )
     
-    # Add serialization rules
+    # association_proxy allows you to easily connect the remainder relationships. the first arg is "what you have many of" the second arg is what you want to pull out of the first arg or instance
+    activities = association_proxy( 'signups', 'activity' )
+
     
     # Add validation
+
+    @validates ('name')
+    def validates_name( self, key, new_name ):
+        if new_name:
+            return new_name
+        raise ValueError('must have name')
     
+    @validates ('age')
+    def validates_age( self, key, new_age ):
+        if 8 <= new_age <= 18:
+            return new_age
+        raise ValueError( 'age must be between 8 & 18' )
     
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -53,14 +78,28 @@ class Camper(db.Model, SerializerMixin):
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    # Add serialization rules
+    serialize_rules = ( '-camper.signups', '-activity.signups', )
+ 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
 
+    # first add these columns to the has many method
+    camper_id = db.Column( db.Integer, db.ForeignKey('campers.id') )
+    activity_id = db.Column( db.Integer, db.ForeignKey('activities.id') )
+
     # Add relationships
-    
-    # Add serialization rules
+    # then add the relationships from the methods you want to connect. back_populates is the variable you will create in the other methods that will join the tables together. 
+
+    camper = db.relationship( 'Camper', back_populates = 'signups' )
+    activity = db.relationship( 'Activity', back_populates = 'signups' )
     
     # Add validation
+    @validates ('time')
+    def validates_time( self, key, new_time ):
+        if 0<= new_time <= 23:
+            return new_time
+        raise ValueError( 'time must be between 0 and 23' )
     
     def __repr__(self):
         return f'<Signup {self.id}>'
